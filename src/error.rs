2d8acb727f599043b70388e1725203590eb9fb68
Georg Brandl <georg@python.org>
2016-05-22 14:26:03 +0200

    No need for several error variants.

--- src/error.rs
+++ src/error.rs
@@ -16,38 +16,28 @@ pub const TIMEOUT_ERROR:  &'static str = "TimeoutError";
 pub const API_ERROR:      &'static str = "APIError";
 pub const SOCKET_ERROR:   &'static str = "SocketError";
 pub const ADDRESS_ERROR:  &'static str = "AddressError";
+pub const ZMQ_ERROR:      &'static str = "ZmqError";
+pub const PROTO_ERROR:    &'static str = "ProtocolError";
 
 
 #[derive(Debug)]
-pub enum Error {
-    Zmq(zmq::Error),
-    Proto(protobuf::ProtobufError),
-    Spin { reason: String, desc: String, origin: String },
+pub struct Error {
+    reason: String,
+    desc:   String,
+    origin: String,
 }
 
 impl Error {
     pub fn into_proto(self) -> pr::Error {
         let mut err = pr::Error::new();
-        match self {
-            Error::Spin { desc, reason, origin } => {
-                err.set_desc(desc);
-                err.set_reason(reason);
-                err.set_origin(origin);
-            },
-            Error::Zmq(ref e) => {
-                err.set_desc(error::Error::description(e).into());
-                err.set_reason("ZmqError".into());
-            },
-            Error::Proto(ref e) => {
-                err.set_desc(error::Error::description(e).into());
-                err.set_reason("ProtocolError".into());
-            }
-        }
+        err.set_desc(self.desc);
+        err.set_reason(self.reason);
+        err.set_origin(self.origin);
         err
     }
 
     pub fn from_proto(mut err: pr::Error) -> Error {
-        Error::Spin {
+        Error {
             reason: err.take_reason(),
             origin: err.take_origin(),
             desc: err.take_desc()
@@ -57,39 +47,40 @@ impl Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match *self {
-            Error::Spin { ref reason, ref desc, .. } => write!(fmt, "{}: {}", reason, desc),
-            _ => write!(fmt, "{}", error::Error::description(self)),
-        }
+        write!(fmt, "{}: {}", self.reason, self.desc)
     }
 }
 
 impl error::Error for Error {
     fn description(&self) -> &str {
-        match *self {
-            Error::Zmq(ref e) => e.description(),
-            Error::Proto(ref e) => e.description(),
-            Error::Spin { ref desc, .. } => desc,
-        }
+        &self.desc
     }
 }
 
 impl From<zmq::Error> for Error {
     fn from(e: zmq::Error) -> Error {
-        Error::Zmq(e)
+        Error {
+            reason: ZMQ_ERROR.into(),
+            desc:   error::Error::description(&e).into(),
+            origin: String::new(),
+        }
     }
 }
 
 impl From<protobuf::ProtobufError> for Error {
     fn from(e: protobuf::ProtobufError) -> Error {
-        Error::Proto(e)
+        Error {
+            reason: PROTO_ERROR.into(),
+            desc:   error::Error::description(&e).into(),
+            origin: String::new(),
+        }
     }
 }
 
 pub type SpinResult<T> = Result<T, Error>;
 
 pub fn spin_err<T>(reason: &str, msg: &str) -> SpinResult<T> {
-    Err(Error::Spin { reason: reason.into(),
-                      desc: msg.into(),
-                      origin: "".into() })
+    Err(Error { reason: reason.into(),
+                desc: msg.into(),
+                origin: String::new() })
 }
